7e3192d7c87606a1d6c2f882d8a876976ca37372
RyoKitatani <ryo0929m@gmail.com>
2021-03-16 14:37:20 +0000

    配送先編集画面作成

--- app/controllers/public/shipping_addresses_controller.rb
+++ app/controllers/public/shipping_addresses_controller.rb
@@ -6,8 +6,24 @@ class Public::ShippingAddressesController < ApplicationController
   end
   
   def create
+    @shipping_address = ShippingAddress.new(shipping_address_params)
+    @shipping_address.customer_id = current_customer
+    @shipping_address.save
+    redirect_to shipping_addresses_path
   end
 
   def edit
+    @shipping_address = ShippingAddress.find(params[:id])
+  end
+  
+  def update
+    @shipping_address = ShippingAddress.find(params[:id])
+    @shipping_address.update(shipping_address_params)
+    redirect_to shipping_addresses_path
+  end
+  
+  private
+  def shipping_address_params
+    params.require(:shipping_address).permit(:shipping_postcode, :shipping_address, :shipping_name)
   end
 end
