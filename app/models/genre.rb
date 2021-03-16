3057ce8626679aab4d1d62f4d7249a7ba61e3e3f
RyoKitatani <ryo0929m@gmail.com>
2021-03-16 07:59:47 +0000

    リレーション反映

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,2 +1,3 @@
 class Genre < ApplicationRecord
+  has_many :items, dependet: :destroy
 end
--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,2 +1,8 @@
 class OrderDetail < ApplicationRecord
+  
+  has_many :items, dependent: :destory
+  belongs_to :order
+  
+  enum order_status: {着手不可:0, 製作待ち:1, 製作中:2, 製作完了:3}
+  
 end
