5bca2830a635b6faecf65b14757ecdef5dfe54ad
RyoKitatani <ryo0929m@gmail.com>
2021-03-16 13:40:56 +0000

    配送先一覧作成

--- app/controllers/public/shipping_addresses_controller.rb
+++ app/controllers/public/shipping_addresses_controller.rb
@@ -1,5 +1,11 @@
 class Public::ShippingAddressesController < ApplicationController
   def index
+    @shipping_address = ShippingAddress.new
+    @customer = current_customer
+    @shipping_addresses = @customer.shipping_addresses
+  end
+  
+  def create
   end
 
   def edit
