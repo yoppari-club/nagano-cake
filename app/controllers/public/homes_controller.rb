596c1df3f2ee4cd9d21415e2d13afb440cad5c52
masumi-soga <sg.kse777@gmail.com>
2021-03-17 05:58:32 +0000

    top,about作成・no-imageアイコン追加

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,5 +1,7 @@
 class Public::HomesController < ApplicationController
   def top
+    # default_scope -> {order(created_at: desc)}
+    @items = Item.all.order(created_at: :desc)
   end
 
   def about
