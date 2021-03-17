6eaf2c4a36a837db532f12da9a4fb8dc96910546
RyoKitatani <ryo0929m@gmail.com>
2021-03-17 13:01:35 +0000

    validation設定

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -9,8 +9,12 @@ class Admin::ItemsController < ApplicationController
   
   def create
     @item = Item.new(item_params)
-    @item.save
-    redirect_to admin_items_path
+    if @item.save
+      flash[:notice] = "商品の新規登録が完了しました。"
+      redirect_to admin_items_path
+    else
+      render :new
+    end
   end
 
   def show
@@ -23,8 +27,12 @@ class Admin::ItemsController < ApplicationController
   
   def update
      @item = Item.find(params[:id])
-     @item.update(item_params)
-     redirect_to admin_item_path(@item)
+     if @item.update(item_params)
+        flash[:notice] = "商品詳細の変更が完了しました。"
+        redirect_to admin_item_path(@item)
+     else
+       render :edit
+     end
   end  
 
   private
