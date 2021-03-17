01b07749416e1218ee2e2513befffde8f4f3c8d0
RyoKitatani <ryo0929m@gmail.com>
2021-03-17 10:14:55 +0000

    indexページへのnewボタン追加

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,14 +1,30 @@
 class Admin::ItemsController < ApplicationController
   def index
-    @items = Item.all  
+    @items = Item.page(params[:page]).reverse_order 
   end
 
   def new
+    @item = Item.new
+  end
+  
+  def create
+    @item = Item.new(item_params)
   end
 
   def show
+    @item = Item.find(params[:id])
   end
 
   def edit
+    
   end
+  
+  def update
+  end  
+
+  private
+  def item_params
+    params.require(:item).permit(:image_id, :name, :description, :genre, :price_excluding_tax, :sales_status)
+  end
+    
 end
