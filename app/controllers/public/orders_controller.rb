acaa1e94b944da0ec507fc460ba5a63ab1d6ceb2
dym330 <cat.of.attic@gmail.com>
2021-03-17 16:46:17 +0000

    confirmアクション追加

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,11 +1,50 @@
 class Public::OrdersController < ApplicationController
+  include Public::OrdersHelper
+
   def new
     @order = Order.new
     @shipping_addresses = current_customer.shipping_addresses
-    @orders = Order.all
   end
 
   def confirm
+    @order = Order.new
+    @cart_items = CartItem.where(customer_id: current_customer.id)
+    customer = current_customer
+    address_option = params[:order][:address_option].to_i
+
+    @order.payment_option = params[:order][:payment_option].to_i
+    if address_option == 0
+      @order.order_in_postcode_address_name(customer.postcode, customer.address, customer.last_name)
+    elsif address_option == 1
+      shipping = ShippingAddress.find(params[:order][:registration_shipping_address])
+      @order.order_in_postcode_address_name(shipping.shipping_postcode, shipping.shipping_address, shipping.shipping_name)
+    elsif address_option == 2
+      @order.order_in_postcode_address_name(params[:order][:shipping_postcode], params[:order][:shipping_address], params[:order][:shipping_name])
+    else
+    end
+    # render plain: @order.inspect
+  end
+
+  def create
+    @order = Order.new(order_params)
+    @order.customer_id = current_customer.id
+    @order.shipping_fee = 800
+    if @order.save
+      @cart_items = CartItem.where(customer_id: current_customer.id)
+      @cart_items.each do |cart_item|
+        order_detail = OrderDetail.new
+        order_detail.item_id = cart_item.item_id
+        order_detail.order_id = @order.id
+        order_detail.amount = cart_item.amount
+        order_detail.price_including_tax = change_price_excluding_tax(cart_item.item.price_excluding_tax)
+        order_detail.production_status = 0
+        if order_detail.save
+          @cart_items.destroy_all
+        end
+      end
+      redirect_to orders_thanks_path
+    else
+    end
   end
 
   def thanks
@@ -16,4 +55,11 @@ class Public::OrdersController < ApplicationController
 
   def index
   end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:shipping_postcode, :shipping_address, :shipping_name, :total_payment, :payment_option)
+  end
+
 end
