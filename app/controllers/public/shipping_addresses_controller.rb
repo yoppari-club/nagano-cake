1c2b72e3eb784233b6b9df5d00f02b36ddc5b715
RyoKitatani <ryo0929m@gmail.com>
2021-03-17 01:38:42 +0000

    create及びdestroyアクション実装

--- app/controllers/public/shipping_addresses_controller.rb
+++ app/controllers/public/shipping_addresses_controller.rb
@@ -7,7 +7,7 @@ class Public::ShippingAddressesController < ApplicationController
   
   def create
     @shipping_address = ShippingAddress.new(shipping_address_params)
-    @shipping_address.customer_id = current_customer
+    @shipping_address.customer_id = current_customer.id
     @shipping_address.save
     redirect_to shipping_addresses_path
   end
@@ -21,6 +21,14 @@ class Public::ShippingAddressesController < ApplicationController
     @shipping_address.update(shipping_address_params)
     redirect_to shipping_addresses_path
   end
+  
+  def destroy
+    @shipping_address = ShippingAddress.find(params[:id])
+    @shipping_address.customer_id = current_customer.id
+    @shipping_address.destroy
+    redirect_to shipping_addresses_path
+  end
+  
   
   private
   def shipping_address_params
