fcbd6b470d15bca82148e57949b311a0370aa1a7
akahito1006 <nekodora5k@gmail.com>
2021-03-17 06:53:16 +0000

    退会済み会員がログインできなくなる仕様wo 追加

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,4 +1,5 @@
 class Public::CustomersController < ApplicationController
+    
   def show
     @customer = current_customer
   end
--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -2,6 +2,7 @@
 
 class Public::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
+  before_action :reject_withdraw_customer, only: [:create]
 
   # GET /resource/sign_in
   # def new
@@ -24,4 +25,12 @@ class Public::SessionsController < Devise::SessionsController
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
+  
+  def reject_withdraw_customer
+    @customer = current_customer
+    if @customer.withdrawal_status == true
+      reset_session
+      redirect_to new_customer_session_path
+    end
+  end
 end
