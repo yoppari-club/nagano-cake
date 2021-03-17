33048cbb9de35d4e2c1f72c377edb4ce8bd8df7b
dym330 <cat.of.attic@gmail.com>
2021-03-17 05:17:44 +0000

    destroyアクション記載

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,17 +1,39 @@
 class Public::CartItemsController < ApplicationController
+  before_action :authenticate_customer!
+
   def index
-    @cart_items = CartItem.all
+    @cart_items = CartItem.where(customer_id: current_customer.id)
   end
-  
+
   def create
   end
-  
+
   def update
+    @cart_item = CartItem.find(params[:id])
+    if @cart_item.update(cart_item_params)
+      flash[:success] = "変更しました"
+      redirect_back(fallback_location: root_path)
+    else
+      flash[:alert] = "正しい個数を入力してください"
+      redirect_back(fallback_location: root_path)
+    end
   end
-  
+
   def destroy
+    @cart_item = CartItem.find(params[:id])
+    @cart_item.destroy
+    redirect_back(fallback_location: root_path)
   end
-  
+
   def destroy_all
+    CartItem.where(customer_id: current_customer.id).destroy_all
+    flash[:success] = "カートの中身を空にしました"
+    redirect_back(fallback_location: root_path)
+  end
+
+  private
+
+  def cart_item_params
+    params.require(:cart_item).permit(:item_id, :amount)
   end
 end
--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,6 +1,7 @@
 class CartItem < ApplicationRecord
-  
+  validates :amount, numericality: { greater_than: 0 }
+
   belongs_to :customer
   belongs_to :item
-  
+
 end
