6b30db29b1d19add95bd24d92aeee7bef975ea6c
RyoKitatani <ryo0929m@gmail.com>
2021-03-17 12:27:46 +0000

    new+edit+show完成

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,6 +1,6 @@
 class Admin::ItemsController < ApplicationController
   def index
-    @items = Item.page(params[:page]).reverse_order 
+    @items = Item.page(params[:page]).reverse_order
   end
 
   def new
@@ -9,6 +9,8 @@ class Admin::ItemsController < ApplicationController
   
   def create
     @item = Item.new(item_params)
+    @item.save
+    redirect_to admin_items_path
   end
 
   def show
@@ -16,15 +18,18 @@ class Admin::ItemsController < ApplicationController
   end
 
   def edit
-    
+     @item = Item.find(params[:id])
   end
   
   def update
+     @item = Item.find(params[:id])
+     @item.update(item_params)
+     redirect_to admin_item_path(@item)
   end  
 
   private
   def item_params
-    params.require(:item).permit(:image_id, :name, :description, :genre, :price_excluding_tax, :sales_status)
+    params.require(:item).permit(:image, :name, :description, :genre_id, :price_excluding_tax, :sales_status)
   end
     
 end
