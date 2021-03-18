a46956245a4893bff18bb65f452310074997982f
masumi-soga <sg.kse777@gmail.com>
2021-03-18 09:10:07 +0000

    admin注文履歴一覧、注文詳細未完成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,11 +1,12 @@
 class Item < ApplicationRecord
-  
+
   has_many :cart_items, dependent: :destroy
   has_many :order_details, dependent: :destroy
   belongs_to :genre
-  
+  has_many :orders, through: :order_details
+
   attachment :image
-  
+
   validates :name, presence: true
   validates :description, presence: true
   validates :genre, presence: true
