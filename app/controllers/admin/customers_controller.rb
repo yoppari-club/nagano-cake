7a41b4064739568ec39b8acda42dcb81cbb01272
akahito1006 <nekodora5k@gmail.com>
2021-03-18 02:20:42 +0000

    admin 会員情報一覧、編集アクション追加

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -5,8 +5,22 @@ class Admin::CustomersController < ApplicationController
   end
 
   def show
+    @customer = Customer.find_by(params[:id])
   end
 
   def edit
+    @customer = Customer.find_by(params[:id])
+  end
+  
+  def update
+    @customer = Customer.find_by(params[:id])
+    p "----------"
+    p @customer
+    @customer.update(customer_params)
+  end
+  
+  private
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postcode, :address, :tel, :email, :withdrawal_status)
   end
 end
