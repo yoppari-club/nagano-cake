0c6699b418161f584af6cf54744c5b746556255d
akahito1006 <nekodora5k@gmail.com>
2021-03-19 04:35:40 +0000

    管理者用ページの閲覧制限

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,11 +1,11 @@
 class Public::CustomersController < ApplicationController
-    
+  before_action :authenticate_customer!
+  
   def show
     @customer = current_customer
   end
 
   def unsubscribe
-
   end
   
   def withdraw
