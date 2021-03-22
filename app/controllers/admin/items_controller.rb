388688d5dfd9275c6c00a726565104cf2140309a
RyoKitatani <ryo0929m@gmail.com>
2021-03-22 04:48:47 +0000

    issue修正

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -11,9 +11,10 @@ class Admin::ItemsController < ApplicationController
   def create
     @item = Item.new(item_params)
     if @item.save
-      flash[:notice] = "商品の新規登録が完了しました。"
-      redirect_to admin_items_path
+      flash[:success] = "商品の新規登録が完了しました。"
+      redirect_to admin_item_path(@item)
     else
+      flash[:danger] = "商品の新規登録内容に不備があります。"
       render :new
     end
   end
@@ -29,9 +30,10 @@ class Admin::ItemsController < ApplicationController
   def update
      @item = Item.find(params[:id])
      if @item.update(item_params)
-        flash[:notice] = "商品詳細の変更が完了しました。"
-        redirect_to admin_items_path
+       flash[:success] = "商品詳細の変更が完了しました。"
+       redirect_to admin_item_path(@item)
      else
+       flash[:danger] = "商品詳細の変更内容に不備があります。"
        render :edit
      end
   end  
