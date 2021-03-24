099c4effca1e5ec4bb978ef288f6e07404904c09
RyoKitatani <ryo0929m@gmail.com>
2021-03-24 05:40:18 +0000

    ログイン後追加

--- spec/system/items_spec.rb
+++ spec/system/items_spec.rb
@@ -24,16 +24,42 @@ RSpec.describe "Items", type: :system do
     end
     
     context "商品詳細ページ" do
-      it "商品詳細を選択できる" do
+      it "商品詳細ページに飛べる" do
         visit item_path(item)
         expect(current_path).to eq "/items/1"
       end
+      
       it "ログイン画面へ移動する" do
         visit item_path(item)
-        click_button "commit"
+        click_button "カートに入れる"
         expect(current_path).to eq "/customers/sign_in"
       end
     end
   end
+  
+  describe "ログイン後のテスト" do
+    let(:customer) { create(:customer)}
+    let(:item) { create(:candy)}
+    let(:other_item) { create(:cookie)}
+    
+    before do
+      create(:cake)
+      item
+      visit new_customer_session_path
+      fill_in "customer_email", with: customer.email
+      fill_in "customer_password", with: customer.password
+      click_button "ログイン"
+    end
+    
+    context "商品詳細ページ" do
+      it "商品をカート追加できる" do
+        visit item_path(item)
+        fill_in "個数を選択", with: 2
+        click_button "カートに入れる"
+        expect(current_path).to eq "/cart_items"
+      end
+    end
+  end
+
 end 
   
